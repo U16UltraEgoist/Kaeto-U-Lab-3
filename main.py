081bd16f06ec7b15b704f4145688bdb9a18de49c
THEU16EGOIST <kuba29@setoncatholic.org>
2026-09-23 18:10:10 +0000

    FUNCTION PART 2

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-def feed_the_dog():
-    print("YUMMY")
-def kill_the_cat():
-    print("REST IN PEACE")
-def figther_jet():
-    print("WHOOSH")
+#functions that add 2 numbers
+def add(x, y):
+    print(x + y)
 
-feed_the_dog()
-kill_the_cat()
-figther_jet()
+
+add(3,21)
+#function that subtarcts 2 numbers
+def subtract(x, y):
+    print(x - y)
+
+
+subtract(3,21)
+# function that multiplies 2 numbers
+def multiply(x, y):
+    print(x * y)
+
+
+multiply(3,21)
+#functions that divides 2 numbers 
+def divide(x, y):
+    print(x / y)
+
+
+divide(28,14)
+
+add(23,7)
+subtract(19,7)
+multiply(12,12)
+divide(6,3)
